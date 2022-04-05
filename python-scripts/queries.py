116123ac33ee6ebb085b919f254256d593ce84f4
Jake Mulford <jakemulf@gmail.com>
2022-04-05 14:33:50 -0400

    fixed python queries file

--- python-scripts/queries.py
+++ python-scripts/queries.py
@@ -1,23 +1,23 @@
 # Simple statistics of each sentiment
-from deephaven import Aggregation as agg, as_list
+from deephaven import agg
 
-agg_list = as_list([
-    agg.AggAvg("Avg_Positive = Positive", "Avg_Negative = Negative", "Avg_Neutral = Neutral", "Avg_Compound = Compound"),
-    agg.AggMed("Med_Positive = Positive", "Med_Negative = Negative", "Med_Neutral = Neutral", "Med_Compound = Compound"),
-    agg.AggStd("Std_Positive = Positive", "Std_Negative = Negative", "Std_Neutral = Neutral", "Std_Compound = Compound"),
-])
+agg_list = [
+    agg.avg(cols=["Avg_Positive = Positive", "Avg_Negative = Negative", "Avg_Neutral = Neutral", "Avg_Compound = Compound"]),
+    agg.med(cols=["Med_Positive = Positive", "Med_Negative = Negative", "Med_Neutral = Neutral", "Med_Compound = Compound"]),
+    agg.std(cols=["Std_Positive = Positive", "Std_Negative = Negative", "Std_Neutral = Neutral", "Std_Compound = Compound"]),
+]
 
-built_in_sia_hackernews_analyzed_statistics = built_in_sia_hackernews.aggBy(agg_list)
+built_in_sia_hackernews_analyzed_statistics = built_in_sia_hackernews.agg_by(agg_list, "")
 
 # Positive percent of built in analysis
-built_in_sia_hackernews_positive_percent = built_in_sia_hackernews.update("PositiveCount = Positive > Negative ? 1 : 0")\
-    .aggBy(as_list([agg.AggSum("PositiveCount"), agg.AggCount("TotalCount")]))\
-    .update("PositivePercent = PositiveCount / TotalCount")
+built_in_sia_hackernews_positive_percent = built_in_sia_hackernews.update(["PositiveCount = Positive > Negative ? 1 : 0"])\
+    .agg_by([agg.sum_(cols=["PositiveCount"]), agg.count_("TotalCount")], "")\
+    .update(["PositivePercent = PositiveCount / TotalCount"])
 
 # Positive percent of custom analysis
-custom_sia_hackernews_positive_percent = custom_sia_hackernews.update("PositiveCount = Sentiment.equals(`positive`)")\
-    .aggBy(as_list([agg.AggSum("PositiveCount"), agg.AggCount("TotalCount")]))\
-    .update("PositivePercent = PositiveCount / TotalCount")
+custom_sia_hackernews_positive_percent = custom_sia_hackernews.update(["PositiveCount = Sentiment.equals(`positive`)"])\
+    .agg_by([agg.sum_(cols=["PositiveCount"]), agg.count_("TotalCount")], "")\
+    .update(["PositivePercent = PositiveCount / TotalCount"])
 
 # Join 2 tables on the unique datetime and sentence combination
-hackernews_joined = built_in_sia_hackernews.join(custom_sia_hackernews, "PublishDatetime, RssEntryTitle", "TextSentiment = Sentiment")
+hackernews_joined = built_in_sia_hackernews.join(custom_sia_hackernews, ["PublishDatetime", "RssEntryTitle"], joins=["TextSentiment = Sentiment"])
